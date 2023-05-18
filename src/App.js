d7211dda1ac15da3e5a23c69170a888c9e764ded
Mahaprasad <mahaprasadprusty72@gmail.com>
2023-05-18 10:11:20 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect, useState } from "react";
+import "./App.css";
+import searchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
 
-function App() {
+const API_URL = "http://www.omdbapi.com?apikey=36e3322c";
+
+// const movie1 = {
+//   Title: "Spiderman the Verse",
+//   Year: "2019–",
+//   imdbID: "tt12122034",
+//   Type: "series",
+//   Poster:
+//     "https://m.media-amazon.com/images/M/MV5BNjA2NmZhOGEtZTQ5OS00MDI0LTg4N2UtYTRmOTllM2I2NDlhXkEyXkFqcGdeQXVyNTU4OTE5Nzc@._V1_SX300.jpg",
+// };
+
+const App = () => {
+  const [movies, setmovies] = useState([]);
+  const [searchTerm, setSearchTerm]=useState('');
+
+  const searchmovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setmovies(data.Search);
+  };
+
+  useEffect(() => {
+    searchmovies("iron man");
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>CineCraze</h1>
+
+      <div className="search">
+        <input
+          placeholder="search for movies "
+          value={searchTerm}
+          onChange={(e) => {setSearchTerm(e.target.value)}}
+        />
+        <img src={searchIcon} alt="search" onClick={() => searchmovies(searchTerm)} />
+      </div>
+
+      {movies && movies.length > 0 ? (
+        <div className="container">
+          {movies.map((Element) => {
+            return <MovieCard movie1={Element} />;
+          })}
+        </div>
+      ) : (
+        <div className="empty">
+          <h1>no movies found</h1>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
